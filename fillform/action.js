dad920ec814ed6d73168d58b8ca431dbdeefc555
jagdish-crossml <jagdish@crossml.com>
2021-09-15 18:09:09 +0530

    updated

--- fillform/action.js
+++ fillform/action.js
@@ -1,35 +1,39 @@
 const image = document.getElementById('picture');
 function getElementByXpath(path) {
     return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-  }
-  
-async function process_forms(){  
+}
+
+async function process_forms() {
     const response = await fetch('http://localhost:8000/fields');
     // console.log(response);
     const data = await response.json();
-    console.log(data);
-    image.src=data.picture;
+    // console.log(data[0].url);
+    // console.log(data[0].fields.fname);
+    image.src = data[0].fields.picture;
+
     const response2 = await fetch('http://localhost:8001/fields');
     const data2 = await response2.json();
-    
+    //  debugger
     for (let index = 0; index < data2.length; index++) {
         const element = data2[index];
-        var ele = getElementByXpath(element.xpath)
-        console.log('ele', ele)
-        ele.value = data[element.attribute_name]
-        
+        var ele = getElementByXpath(element.xpath);
+        // console.log('ele', ele);
+        ele.value = data[element.attribute_name];
+        fillforms(data);
+
+
     }
-// (data => {
-//     fillforms(data); 
-//     console.log(data);
-//   });
-}  
+    chrome.storage.local.set({ 'data': data });
+    
+    chrome.runtime.sendMessage({ greeting: 'hello', data: data }, function (response) {
+        console.log(response.received);
+    });
+}
 
 function fillforms(data) {
-    
-    fillField(document.querySelector('input[name="firstname"]'), data.fname);
-    fillField(document.querySelector('input[name="lastname"]'), data.lname);
-    fillField(image, data.picture);
+    fillField(document.querySelector('input[name="firstname"]'), data[0].fields.fname);
+    fillField(document.querySelector('input[name="lastname"]'), data[0].fields.lname);
+    fillField(image, data[0].fields.picture);
 }
 
 function fillField(field, value) {
@@ -38,4 +42,7 @@ function fillField(field, value) {
     }
 }
 
-process_forms()
+process_forms();
+
+
+
