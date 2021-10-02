04dcd043434a6a8ae893ba2813e6dbaef72dfcb4
jagdish-crossml <jagdish@crossml.com>
2021-10-02 14:59:58 +0530

    updated

---
+++ eform_files/js/check.js
@@ -0,0 +1,127 @@
+// const rect = document.getElementById('checkdiv')
+// const checkbox = '<input type="checkbox" id="checktwo">'
+// const text = ["What is your PAN number?", "What is your Social Security Number?", "What is your Email ID?", "What is your Father's name?"]
+//     // for (j = 0; j < 10; j++) {
+// // const name = `<label id="name">${text.map(a=>a[i]).join('')}</label>`
+//     // }
+// const changeimg = '<img id="rotateimg" src="assets/change.png">'
+// const textbox = '<input class="hide" id="textbox" type="text">'
+// const textdiv = '<div id="textdiv" class="hide">'
+// const checkimg = '<i class="fa fa-check-circle" id="rightimg"></i>'
+// const cancelimg = '<i class="fa fa-times-circle" id="wrongimg"></i>'
+// const enddiv = '</div>'
+// var toAdd = document.createDocumentFragment();
+
+// for (i = 0; i < 5; i++) {
+//     var div = document.createElement("div");
+//     div.className = "finalBlock";
+//     div.id = i
+//     div.innerHTML = checkbox + name + changeimg + textbox + textdiv + checkimg + cancelimg + enddiv;
+//     toAdd.appendChild(div)
+// }
+// rect.appendChild(toAdd)
+
+
+
+
+const change = document.querySelectorAll('.rotateimg')
+const span = document.getElementById('data')
+const element1 = document.getElementById('textbox')
+const element2 = document.getElementById('textdiv')
+const close = document.getElementById('wrongimg');
+const showdata = document.getElementById('rightimg');
+change.forEach(item => {
+
+    item.addEventListener('click', function() {
+        console.log('click')
+        const hide1 = function(element1, element2) {
+            element1.classList.toggle('hide')
+            element2.classList.toggle('hide')
+            item.style.display = 'none'
+            element1.style = 'margin-top:-30px'
+            element2.style = 'margin-top:-41px'
+
+
+        }
+        hide1(element1, element2)
+    });
+})
+
+
+
+close.addEventListener('click', function() {
+
+
+    const hideone = function(element1, element2) {
+        element1.classList.toggle('hide')
+        element2.classList.toggle('hide')
+
+        change.forEach(i => {
+            i.style.display = 'block'
+        })
+
+
+    }
+    hideone(element1, element2)
+})
+
+
+showdata.addEventListener('click', function() {
+
+
+    const hideone = function(element1, element2) {
+        element1.classList.toggle('hide')
+        element2.classList.toggle('hide')
+        change.forEach(i => {
+            i.style.display = 'block'
+        })
+        span.innerHTML = element1.value
+
+
+    }
+    hideone(element1, element2)
+
+})
+
+
+document.getElementById('arrow').addEventListener('click', function() {
+    const element = document.getElementById('rectangle1')
+    const hide = function(element) {
+        element.style.display = 'none';
+        document.getElementById('topdiv').style.top = '550px';
+    }
+    const show = function(element) {
+        element.style.display = 'block';
+        document.getElementById('topdiv').style.top = '142px';
+    }
+    const toggle = function(element) {
+        if (window.getComputedStyle(element).display !== 'none') {
+            hide(element);
+            return;
+        }
+
+        // show the element
+        show(element);
+
+    }
+    toggle(element)
+
+});
+
+function checkAll(ele) {
+    var checkboxes = document.getElementsByName('check');
+    if (ele.checked) {
+        for (var i = 0; i < checkboxes.length; i++) {
+            if (checkboxes[i].type == 'checkbox') {
+                checkboxes[i].checked = true;
+            }
+        }
+    } else {
+        for (var i = 0; i < checkboxes.length; i++) {
+
+            if (checkboxes[i].type == 'checkbox') {
+                checkboxes[i].checked = false;
+            }
+        }
+    }
+}
--- eform_files/js/wepage9.js
+++ eform_files/js/wepage9.js
@@ -1,46 +1,83 @@
-$(function() {
-    $('#load').click(function() {
-        $('#box:none').slice(0, 2).show();
-        if ($('#box').length == $('#box:block').length) {
-            $('#load').hide();
-        }
-    });
-});
+// var html = '';
+// const jsondata = async function() {
+//     const response = await fetch('http://localhost:8002/fields');
 
-var html = '';
-const jsondata = async function() {
-    const response = await fetch('http://localhost:8000/fields');
+//     const data = await response.json();
+//     console.log(data.length)
+//     for (i = 0; i < data.length; i++) {
+//         html += `<div id="box"><ul id="datalist">
+//             <li> ${data[i].fields.address} <img id="cpimg" alt="e-form" src="assets/copy.png">
+//             <img id="puimg" alt="e-form" src="assets/pause.png">
+//             <img id="unimg" alt="e-form" src="assets/unpin.png"></li>
+//             <li><img id="raimg" src="assets/rating.png"><label id="number">8.5</label></li>
+//             </ul></div>`;
+//         var container = document.getElementById("datadiv");
+//         container.innerHTML = html;
+//         const btns = document.createElement('button')
+//         btns.innerText = 'Load More'
+//         btns.id = 'load'
+//         btns.className = 'btn btn-primary'
+//         document.getElementById('datadiv').insertAdjacentElement('beforeend', btns)
+//         btns.addEventListener('click', function() {
+//             $(window).scroll(function() {
+//                 if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+//                     // ajax call get data from server and append to the div
 
-    const data = await response.json();
-    console.log(data.length)
-    for (i = 0; i < data.length; i++) {
-        html += `<div id="box"><ul id="datalist">
-            <li> ${data[i].fields.fname} <img id="cpimg" alt="e-form" src="assets/copy.png">
-            <img id="puimg" alt="e-form" src="assets/pause.png">
-            <img id="unimg" alt="e-form" src="assets/unpin.png"></li>
-            <li> ${data[i].fields.lname} </li>
-            <li><img id="raimg" src="assets/rating.png"></li>
-            </ul></div>`;
-        var container = document.getElementById("datadiv");
-        container.innerHTML = html;
-        // contsiner.insertAdjacentHTML('beforeend', '<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>')
+//                 }
+//             });
+//         })
+//     }
 
-    }
+// }
+// jsondata();
 
-}
-jsondata();
+// var html = '';
+// const jsondata = async function() {
+//     const response = await fetch('http://localhost:8002/fields');
+
+//     const data = await response.json();
+//     console.log(data.length)
+//     for (i = 0; i < data.length; i++) {
+
+//         html += `<div id="one">
+//         <p id="text"> ${data[i].fields.address}</p>
+//         <div id="images">
+//                 <i class="far fa-copy" id="cpimg"></i>
+//                 <i class="fas fa-pause" id="puimg"></i>
+//                 <i class="bi bi-pin-angle" id="unimg"></i>
+//             </div>
+//             <div id="group">
+//             <img id="raimg" src="assets/rating.png">
+//             <label id="number"> ${data[i].fields.rating} </label>
+//             </div></div>`;
+//         var container = document.getElementById("datadiv");
+//         container.innerHTML = html;
+//         const btns = document.createElement('button')
+//         btns.innerText = 'Load More'
+//         btns.id = 'load'
+//         btns.className = 'btn btn-primary'
+//         document.getElementById('datadiv').insertAdjacentElement('beforeend', btns)
+//         btns.addEventListener('click', function() {
+//             window.scroll({ top: 100, behavior: 'smooth' })
+
+//         })
+//     }
+
+// }
+// jsondata();
 
 
-
-document.getElementById('arrow').addEventListener('click', function() {
+document.getElementById('topdiv').addEventListener('click', function() {
     const element = document.getElementById('rectangle1')
     const hide = function(element) {
         element.style.display = 'none';
         document.getElementById('topdiv').style.top = '550px';
+        document.getElementById('topdiv').innerHTML = '<img id="eformimg" src="assets/eform.png">'
     }
     const show = function(element) {
         element.style.display = 'block';
         document.getElementById('topdiv').style.top = '142px';
+        document.getElementById('topdiv').innerHTML = '<img id="arrow" src="assets/down.png">'
     }
     const toggle = function(element) {
         if (window.getComputedStyle(element).display !== 'none') {
@@ -55,6 +92,7 @@ document.getElementById('arrow').addEventListener('click', function() {
     toggle(element)
 
 });
+
 
 const search = document.getElementById('srchimg')
 search.addEventListener('click', function() {
@@ -73,15 +111,15 @@ search.addEventListener('click', function() {
 //search function
 var rect = document.getElementById('sdiv');
 async function searchValue() {
-    const response = await fetch('http://localhost:8000/fields');
+    const response = await fetch('http://localhost:8002/fields');
 
     const data1 = await response.json();
-    console.log(data1[0])
+    console.log(data1[0].fields.address)
     document.getElementById('textInput').addEventListener('keyup', function() {
         let pattern = new RegExp(this.value, 'g');
 
-        let resultSet = data1.filter(item => item.fields.fname.match(pattern) && this.value != '').map(item => `<li id="resultset"> ${item.fields.fname} </li>`);
-
+        let resultSet = data1.filter(item => item.fields.address.match(pattern) && this.value != '').map(item => `<div id="resultset"> ${item.fields.address} </div>`);
+        console.log(resultSet)
         document.getElementById('ismyModal').innerHTML = resultSet;
     });
 }
